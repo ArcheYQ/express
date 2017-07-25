e13723454ddddce5bb37f9d6a4e180af2946e9ef
ArcheYQ <951342519@qq.com>
2017-07-25 15:55:23 +0800

    黄雅倩是小仙女

--- app/src/main/java/com/express/activity/LoginActivity.java
+++ app/src/main/java/com/express/activity/LoginActivity.java
@@ -3,9 +3,9 @@ package com.express.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
-import com.bumptech.glide.Glide;
+
 import com.express.R;
-import jp.wasabeef.glide.transformations.BlurTransformation;
+
 import qiu.niorgai.StatusBarCompat;
 
 public class LoginActivity extends AppCompatActivity {
@@ -16,9 +16,9 @@ public class LoginActivity extends AppCompatActivity {
         StatusBarCompat.translucentStatusBar(this);
         setContentView(R.layout.activity_login);
         ImageView imageView = (ImageView) findViewById(R.id.iv_login_background);
-        Glide.with(this)
-                .load(R.drawable.account)
-                .bitmapTransform(new BlurTransformation(this, 23, 4))//高斯模糊
-                .into(imageView);
+//        Glide.with(this)
+//                .load(R.drawable.mainbg)
+//                .bitmapTransform(new BlurTransformation(this, 23, 4))//高斯模糊
+//                .into(imageView);
     }
 }
