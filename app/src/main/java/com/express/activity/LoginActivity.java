7c477d6ce760abb3608184ebee71085970a40d7d
ArcheYQ <951342519@qq.com>
2017-09-05 20:37:29 +0800

    .

--- app/src/main/java/com/express/activity/LoginActivity.java
+++ app/src/main/java/com/express/activity/LoginActivity.java
@@ -1,11 +1,18 @@
 package com.express.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,6 +41,14 @@ public class LoginActivity extends BaseActivity {
     EditText etLoginAccount;
     @Bind(R.id.et_login_password)
     EditText etLoginPassword;
+    @Bind(R.id.iv_login_background)
+    ImageView ivLoginBackground;
+    @Bind(R.id.tv_forget)
+    TextView tvForget;
+    @Bind(R.id.tv_loginRule)
+    TextView tvLoginRule;
+    @Bind(R.id.iv_into)
+    ImageView ivInto;
     private CompositeSubscription mCompositeSubscription;
 
     @Override
@@ -51,14 +66,31 @@ public class LoginActivity extends BaseActivity {
                         || actionId == EditorInfo.IME_ACTION_DONE
                         || (event != null && KeyEvent.KEYCODE_ENTER == event.getKeyCode()
                         && KeyEvent.ACTION_DOWN == event.getAction())) {
-                    showProgressDialog();
-                    User user = new User();
-                    user.setUsername(etLoginAccount.getText().toString());
-                    user.setPassword(etLoginPassword.getText().toString());
-                    loginBmob(user);
+
                     return true;
                 }
                 return false;
+            }
+        });
+        etLoginPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if(!TextUtils.isEmpty(etLoginPassword.getText().toString())){
+                ivInto.setVisibility(View.VISIBLE);
+
+            }else{
+                ivInto.setVisibility(View.INVISIBLE);
+            }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
     }
@@ -112,7 +144,7 @@ public class LoginActivity extends BaseActivity {
                     startActivity(new Intent(LoginActivity.this, MainActivity.class));
                     finish();
                 } else {
-                    Toast.makeText(mActivity, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(mActivity, "请确认账号密码是否正确", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -142,9 +174,51 @@ public class LoginActivity extends BaseActivity {
                 .subscribe(subscriber));
     }
 
-    @OnClick(R.id.tv_register)
-    public void onViewClicked() {
-        showProgressDialog();
-        login(etLoginAccount.getText().toString(),etLoginPassword.getText().toString());
+
+    @OnClick({R.id.tv_register, R.id.tv_forget, R.id.tv_loginRule,R.id.iv_into})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_forget:
+                break;
+            case R.id.tv_loginRule:
+                showNormalDialog();
+                break;
+            case R.id.tv_register:
+                showProgressDialog();
+                login(etLoginAccount.getText().toString(), etLoginPassword.getText().toString());
+                break;
+            case R.id.iv_into:
+                showProgressDialog();
+                User user = new User();
+                user.setUsername(etLoginAccount.getText().toString());
+                user.setPassword(etLoginPassword.getText().toString());
+                loginBmob(user);
+                break;
+        }
     }
+
+    private void showNormalDialog() {
+        /* @setIcon 设置对话框图标
+         * @setTitle 设置对话框标题
+         * @setMessage 设置对话框消息提示
+         * setXXX方法返回Dialog对象，因此可以链式设置属性
+         */
+        final AlertDialog.Builder normalDialog =
+                new AlertDialog.Builder(this);
+        normalDialog.setIcon(R.drawable.rule);
+        normalDialog.setTitle("登录说明n(*≧▽≦*)n");
+        normalDialog.setMessage("密码说明：密码默认为身份证后6位。若在工大助手上改过密码，请使用更改后的密码登录。\n首次登录的同学请点击第一次登录，否则无法登录。");
+        normalDialog.setPositiveButton("确定",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //...To-do
+                    }
+                });
+
+        // 显示
+        normalDialog.show();
+    }
+
+
 }
